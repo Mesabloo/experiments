30dd2ab6972873e55d69f9c619c09e725840147d
Mesabloo <22964017+Mesabloo@users.noreply.github.com>
2020-04-26 22:07:22 +0200

    WIP: trying to learn comonads

--- ambient-auth/app/Main.hs
+++ ambient-auth/app/Main.hs
@@ -1,6 +1,41 @@
 module Main where
 
-import Lib
+import Control.Comonad
+import Control.Comonad.Env
+
+import System.IO.Unsafe (unsafePerformIO)
+
+-- Auths (assume constructors are not exported)
+data SystemAuth = SystemAuth
+data OutputAuth = OutputAuth
+data InputAuth  = InputAuth
+
+deriveOutputAuth :: SystemAuth -> OutputAuth
+deriveOutputAuth _ = OutputAuth
+
+deriveInputAuth :: SystemAuth -> InputAuth
+deriveInputAuth _ = InputAuth
+
+readLine :: Env InputAuth () -> String
+readLine _ = unsafePerformIO getLine
+
+putLine :: Env OutputAuth String -> ()
+putLine e = unsafePerformIO (putStrLn (extract e))
+
+system :: Env SystemAuth ()
+system = env SystemAuth ()
+
+-------------------------------------------------------------------
+---- Main logic (assume in another module)
 
 main :: IO ()
-main = someFunc
+main = do
+  let (_, res) = runEnv (system =>> main')
+  pure res
+
+main' :: Env SystemAuth () -> ()
+main' auth =
+  readLine =>> liftWith deriveInputAuth _
+
+liftWith :: (auth -> auth') -> (Env auth' a -> b) -> (Env auth a -> b)
+liftWith f g env' = g (local f env')
